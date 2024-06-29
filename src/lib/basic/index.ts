b6e4d03aa3ac467ba60a6a40f1058c2e68f23a4a
plutoniumm <51614345+plutoniumm@users.noreply.github.com>
2024-06-29 23:12:11 +0530

    make fucking fetch happen

--- src/lib/basic/index.ts
+++ src/lib/basic/index.ts
@@ -1,14 +1,33 @@
 import { MarkdownParser, defaultMarkdownParser } from "prosemirror-markdown";
-import { keymap } from "prosemirror-keymap";
-import { schema } from "./schema";
-
 import { mathPlugin } from "@benrbray/prosemirror-math";
-import { history } from "prosemirror-history";
 import { baseKeymap } from "prosemirror-commands";
-
-import { buildKeymap } from "./keymap";
+import { DOMParser } from "prosemirror-model";
+import { history } from "prosemirror-history";
+import { keymap } from "prosemirror-keymap";
 import { buildInputRules } from "./inputs";
 import { shikiLazyPlugin } from "./code";
+import { buildKeymap } from "./keymap";
+import { schema } from "./schema";
+
+const pm_dp = DOMParser.fromSchema(schema);
+let dp = null;
+export function parsed (string: string, display: boolean = false) {
+  if (!dp) dp = new window.DOMParser();
+  let mode = display ? "math-display" : "math-inline";
+  let wrap = display ? "div" : "span";
+
+  let math = `<${wrap}><${mode}>${string}</${mode}></${wrap}>`;
+  let parsed = dp.parseFromString(math, "text/html").body.firstChild;
+
+  parsed = pm_dp.parse(parsed);
+
+  // if we're in inline and the parsed is a paragraph, return the first child
+  if (!display && parsed.content.childCount === 1) {
+    return parsed.content.firstChild.content.content[0];
+  } else {
+    return parsed;
+  }
+};
 
 export const parser = new MarkdownParser(
   schema,
--- src/lib/editor.svelte
+++ src/lib/editor.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { defaultMarkdownSerializer } from "prosemirror-markdown";
-  import { parser, plugins } from "$lib/basic";
+  import { parser, plugins, parsed } from "$lib/basic";
   import { EditorState } from "prosemirror-state";
   import { EditorView } from "prosemirror-view";
   import { onMount } from "svelte";
@@ -8,10 +8,40 @@
   let { value = $bindable(), raw = $bindable() } = $props();
   let editor: N<HTMLElement> = null;
 
+  const M_INLINE = /\$(.+)\$/g;
+  const M_BLOCK = /\$\$(.+)\$\$/g;
+  const isInline = (s: string) => M_INLINE.test(s);
+  const isBlock = (s: string) => M_BLOCK.test(s);
+
   onMount(() => {
     let state = EditorState.create({
       doc: parser.parse(value),
       plugins,
+    });
+
+    // for each node check if it's text
+    state.doc.descendants((node, pos) => {
+      if (node.isText) {
+        node.text = node?.text as string;
+        if (isBlock(node.text)) {
+          const math = node.text.replace(M_BLOCK, "$1").trim();
+          const parse = parsed(math, true);
+
+          const start = pos;
+          const end = start + node.text.length;
+          const tr = state.tr.replaceWith(start, end, parse);
+          state = state.apply(tr);
+        } else if (isInline(node.text)) {
+          // @ts-ignore
+          const math = new RegExp(M_INLINE).exec(node.text)[1];
+          const parse = parsed(math, false);
+
+          const start = pos + node.text.indexOf("$");
+          const end = start + math.length + 2;
+          const tr = state.tr.replaceWith(start, end, parse);
+          state = state.apply(tr);
+        }
+      }
     });
 
     let view = new EditorView(editor, {
